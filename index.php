0379319c0caf59ee4cc052f6a896b565ed2dc6e0
Kiharaten <sukerock324@gmail.com>
2020-12-02 11:37:23 +0900

    階層編集

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>hal motor root index</h1>
+<p>最終的にはこの画面は表示されず、自動的に顧客側ログインに飛ばされるようになると思います。</p>
+    <ul>
+        <li><a href="admin/index.php">admin_side</a></li>
+        <li><a href="customer/index.php">customer_side</a></li>
+        <li><a href="sample/adminHeader.html">sample_side</li>
+    </ul>
+</body>
+</html>
