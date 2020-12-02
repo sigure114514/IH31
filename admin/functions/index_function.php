32ce9d55c59d1710babbe6c822a768c0ca4ea154
Kiharaten <sukerock324@gmail.com>
2020-12-02 12:19:57 +0900

    indexとfunctionの使い方を追加

---
+++ admin/functions/index_function.php
@@ -0,0 +1,17 @@
+<?php
+function load_page_source() {
+// 画面の固定値を戻り値に指定
+    $const = [
+        "title" => "TOP | HALMORTOR",
+        "path_to_logo_image" => "./img/HALMOTOR.png",
+        "regist" => "登録",
+        "matter" => "案件",
+        "change" => "変更・管理",
+        "log_out" => "ログアウト",
+    ];
+    return $const;
+}
+
+function hogehoge($hikisuu) {
+
+}
